a382501328b8c57aad3ab804104600d88069b5ac
laigor <leaigor@gmail.com>
2025-07-29 16:57:27 +0300

    Debug

--- meilisync/settings.py
+++ meilisync/settings.py
@@ -47,7 +47,7 @@ class Sync(BasePlugin):
     full: bool = False
     index: str | None = None
     fields: dict | None = None
-    source: any = None  # или Source, если хотите типизацию
+    source: Source | None = None  # или Source, если хотите типизацию
 
     @property
     def index_name(self):
